60768db27d24cc2b715b35f9b212927186256869
moad <mouad3435@gmail.com>
2024-01-22 13:10:41 -0800

    2nd commit for the first task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             count = count + 1
-        except IndexError as e:
+        except IndexError:
             continue
     print("")
     return count
